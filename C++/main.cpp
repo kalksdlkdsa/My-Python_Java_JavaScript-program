9b58561671d125abf0c43c8450f38f8005c0e4c1
kalksdlkdsa <156580271+kalksdlkdsa@users.noreply.github.com>
2024-01-14 20:36:55 +0700

    My new file

---
+++ C++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//n bit => -2^(n-1) -> 2^(n-1) - 1
+//
+//Kieu du lieu so:
+//So nguyen
+//int: 4byte = 32bit : -2^31 -> 2^31 - 1
+//So thuc
+
+int main() {
+    cout << INT_MIN << " " << INT_MAX << endl;
+    return 0;
+}
