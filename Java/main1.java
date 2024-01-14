7f30acf2bca6bc0f2ef1d3b5b30b3615f1e3171c
kalksdlkdsa <156580271+kalksdlkdsa@users.noreply.github.com>
2024-01-14 21:45:08 +0700

    Delete Java/main1.java

--- Java/main1.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-public class main1 {
-    public static void main(String[] args) {
-        System.out.println(LocalDateTime.now());
-        System.out.println(LocalDateTime.of(+999_999_999,12,31,23,59,59,999_999_999));
-        System.out.println(LocalDateTime.of(0,1,1,0,0,0,0));
-        System.out.println(LocalDateTime.of(-999_999_999,1,1,0,0,0,0));
-    }
-}
